8b803d3677f3dabe0fadb51a457e0ca2cb556500
Bibashthapa <101507407+Bibashthapa@users.noreply.github.com>
2022-04-01 07:36:40 +0545

    Add files via upload

---
+++ table.php
@@ -0,0 +1,33 @@
+<?php
+$host="containers-us-west-33.railway.app:6540";
+$user="root";
+$password="7T5kqa4KUlI07WQjJ6KA";
+$db="railway";
+$conn=mysqli_connect($host,$user,$password,$db);
+
+if ($conn)
+{
+	echo "yes";
+}
+else
+{
+	echo "sorry sir";
+	
+	
+}
+
+$sql= "INSERT INTO tb_1(name,comment)
+VALUES ('handacy', 'my second comment')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else 
+{
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+
+
+?>
